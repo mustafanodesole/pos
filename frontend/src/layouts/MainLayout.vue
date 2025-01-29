45ad5ba6bb5bfc0101b1b2029ab5523d6f8e7958
tritoncg <admin@tritoncg.com>
2025-01-29 20:38:06 +0000

    dark mode added

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,10 +1,26 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header class=" text-black" style="background-color: #D4E5F5;">
+    <q-header
+      class="text-black"
+      :style="{ 'background-color': $q.dark.isActive ? '#1D1D1D' : '#d4e5f5' }"
+    >
       <q-toolbar>
-        <q-btn color="black" flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+        <q-btn
+          :color="$q.dark.isActive ? 'white' : 'black'"
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
 
-        <q-toolbar-title style="font-family:Arial, Helvetica, sans-serif"> AMS </q-toolbar-title>
+        <q-toolbar-title
+          style="font-family: Arial, Helvetica, sans-serif"
+          :style="{ color: $q.dark.isActive ? 'white' : 'black' }"
+        >
+          AMS
+        </q-toolbar-title>
 
         <!-- <div class="">
           <q-input dense rounded outlined placeholder="Search Your Product">
@@ -14,16 +30,42 @@
 </q-input>
 
 </div> -->
+        <q-toggle
+          v-model="$q.dark.isActive"
+          label=""
+          :icon="$q.dark.isActive ? 'brightness_6' : 'dark_mode'"
+          color="white"
+          icon-color="black"
+        />
         <div class="q-px-md">
-          <q-btn v-if="route.path == '/'" class="text-capitalize text-subtitle2" label="Add Products"
-            style="background-color: #0D1721;" text-color="white" to="/add" />
-          <q-btn v-if="route.path == '/add'" class="text-capitalize text-subtitle2" label="View Products"
-            style="background-color: #0D1721;" text-color="white" to="/" />
+          <q-btn
+            v-if="route.path == '/'"
+            class="text-capitalize text-subtitle2"
+            label="Add Products"
+            style="background-color: #0d1721"
+            text-color="white"
+            :outline="$q.dark.isActive"
+            to="/add"
+          />
+          <q-btn
+            v-if="route.path == '/add'"
+            class="text-capitalize text-subtitle2"
+            label="View Products"
+            style="background-color: #0d1721"
+            text-color="white"
+            to="/"
+          />
         </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered class=" text-white" style="background-color: #0D1721;">
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      class="text-white"
+      style="background-color: #0d1721"
+    >
       <q-list>
         <q-item-label header class="text-white"> Point of Sale </q-item-label>
         <q-separator horizontal />
@@ -31,7 +73,10 @@
       </q-list>
     </q-drawer>
 
-    <q-page-container class="gradient">
+    <q-page-container
+      :class="!$q.dark.isActive && 'gradient'"
+      :style="{ 'background-color': $q.dark.isActive ? '#1D1D1D' : '#d4e5f5' }"
+    >
       <router-view />
     </q-page-container>
   </q-layout>
@@ -41,6 +86,9 @@
 import { ref } from "vue";
 import EssentialLink from "components/EssentialLink.vue";
 import { useRoute } from "vue-router";
+import { useQuasar } from "quasar";
+const $q = useQuasar();
+
 const route = useRoute();
 console.log(route.path);
 
@@ -64,6 +112,5 @@ function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
 }
 </script>
-
 
 <style scoped></style>
--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,83 +1,160 @@
 <template>
   <q-page>
-  <div class="q-pa-md">
-    <q-table @row-click="handleRowClick" class="text-h4 text-capitalize " flat title="Products"
-      dense :rows="filterData" :columns="columns" row-key="name" :grid="$q.screen.lt.sm" :rows-per-page-options="[10, 15 , 20 , 25, 30]">
-      <template v-slot:top-right>
-        <div class="flex flex-center">
-          <q-input outlined dense v-model="searchProduct"  debounce="300" placeholder="Search by Name" style="background-color: #F2F5F8;">
-            <template v-slot:append>
-              <q-icon name="search" />
-            </template>
-          </q-input>
-          <q-btn class="q-ml-sm" label="Export CSV"  @click="exportToCSV" color="green-10"/>
-        </div>
-      </template>
-
-
-
-      <!-- Scoped slot for Action column -->
-      <template v-slot:body-cell-Action="props">
-        <Td>
-          <div class="row items-center justify-center">
-            <q-btn v-if="props.row.isSell" icon="close" flat class="" size="sm"
-              @click.stop="props.row.isSell = false" />
-            <q-input v-if="props.row.isSell" v-model="sellItems" @click.stop type="number" dense outlined class="col-2 "
-              placeholder="No. of Items" />
-            <q-btn v-if="props.row.isSell" flat icon="send_and_archive" color="black"
-              @click.stop="handleSellItems(props.row)" class="col-1" />
-
-            <q-btn v-if="!props.row.isSell" flat icon="sell" color="black" @click.stop="toggleSellInput(props.row)"
-              class="col-6">
-              <q-tooltip class="bg-black" anchor="top middle" self="bottom middle" :offset="[10, 10]">
-                <strong class="text-capitalize">{{ props.row.product_name }}</strong>
-              </q-tooltip>
-            </q-btn>
+    <div class="q-pa-md">
+      <q-table
+        @row-click="handleRowClick"
+        class="text-h4 text-capitalize"
+        flat
+        title="Products"
+        dense
+        :rows="filterData"
+        :columns="columns"
+        row-key="name"
+        :grid="$q.screen.lt.sm"
+        :rows-per-page-options="[10, 15, 20, 25, 30]"
+      >
+        <template v-slot:top-right>
+          <div class="flex flex-center">
+            <q-input
+              outlined
+              dense
+              v-model="searchProduct"
+              debounce="300"
+              placeholder="Search by Name"
+              :style="{ 'background-color': $q.dark.isActive ? '#1D1D1D' : '#d4e5f5' }"
+            >
+              <template v-slot:append>
+                <q-icon name="search" />
+              </template>
+            </q-input>
+            <q-btn
+              icon="download"
+              class="q-ml-sm"
+              label="Export"
+              @click="exportToCSV"
+              :color="$q.dark.isActive ? 'white' : 'black'"
+              outline
+              rounded
+            />
           </div>
-        </Td>
-      </template>
+        </template>
 
+        <template v-slot:body-cell-profit="props">
+          <Td class="text-center">
+            <span :class="props.row.profit < 0 ? 'text-red' : ''">
+              Rs. {{ props.row.profit }} /-
+            </span>
+          </Td>
+        </template>
 
+        <!-- Scoped slot for Action column -->
+        <template v-slot:body-cell-Action="props">
+          <Td>
+            <div class="row items-center justify-center">
+              <q-btn
+                v-if="props.row.isSell"
+                icon="close"
+                flat
+                class=""
+                size="sm"
+                @click.stop="props.row.isSell = false"
+              />
+              <q-input
+                v-if="props.row.isSell"
+                v-model="sellItems"
+                @click.stop
+                type="number"
+                dense
+                outlined
+                class="col-2"
+                placeholder="No. of Items"
+              />
+              <q-btn
+                v-if="props.row.isSell"
+                flat
+                icon="send_and_archive"
+                :color="$q.dark.isActive ? 'white' : 'black'"
+                @click.stop="handleSellItems(props.row)"
+                class="col-1"
+              />
 
-    </q-table>
+              <q-btn
+                v-if="!props.row.isSell"
+                flat
+                icon="sell"
+                :color="$q.dark.isActive ? 'white' : 'black'"
+                @click.stop="toggleSellInput(props.row)"
+                class="col-6"
+              >
+                <q-tooltip
+                  class="bg-black"
+                  anchor="top middle"
+                  self="bottom middle"
+                  :offset="[10, 10]"
+                >
+                  <strong class="text-capitalize">{{ props.row.product_name }}</strong>
+                </q-tooltip>
+              </q-btn>
+            </div>
+          </Td>
+        </template>
+      </q-table>
 
-    <q-dialog v-model="isRow" persistent>
-      <q-card style="min-width: 350px">
-        <q-card-section class="flex justify-between">
-          <div class="text-h6 text-capitalize">{{ selectedRow?.product_name || "Product Name" }}</div>
-          <q-btn icon="close" v-close-popup @click="isRow = false" flat />
-        </q-card-section>
+      <q-dialog v-model="isRow" persistent>
+        <q-card style="min-width: 350px">
+          <q-card-section class="flex justify-between">
+            <div class="text-h6 text-capitalize">
+              {{ selectedRow?.product_name || "Product Name" }}
+            </div>
+            <q-btn icon="close" v-close-popup @click="isRow = false" flat />
+          </q-card-section>
 
-        <q-card-section class="q-pt-none q-gutter-y-md">
-          <q-input v-model="purchasedPrice" type="number" outlined label="Purchased Price" />
-          <q-input v-model="retailPrice" type="number" outlined label="Retail Price" />
-        </q-card-section>
+          <q-card-section class="q-pt-none q-gutter-y-md">
+            <q-input
+              v-model="purchasedPrice"
+              type="number"
+              outlined
+              label="Purchased Price"
+            />
+            <q-input v-model="retailPrice" type="number" outlined label="Retail Price" />
+            <q-input
+              v-model="totalItems"
+              type="number"
+              outlined
+              label="No. of Purchased Items"
+            />
+          </q-card-section>
 
-        <q-card-actions align="right" class="text-primary">
-          <q-btn outline label="Cancel" v-close-popup color="red"  />
-          <q-btn label="Update" @click="handleUpdatePrice" v-close-popup color="black" />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-  </div>
-</q-page>
+          <q-card-actions align="right" class="text-primary">
+            <q-btn outline label="Cancel" v-close-popup color="red" />
+            <q-btn
+              label="Update"
+              @click="handleUpdatePrice"
+              v-close-popup
+              color="black"
+            />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
+    </div>
+  </q-page>
 </template>
 
 <script setup>
 import { useQuasar } from "quasar";
-import {onMounted,  computed } from "vue";
+import { onMounted, computed } from "vue";
 import { ref } from "vue";
 const $q = useQuasar();
 const isRow = ref(false);
 const searchProduct = ref("");
-const sellItems = ref(null)
+const sellItems = ref(null);
 const rows = ref([]);
-const sellingId = ref(null)
+const sellingId = ref(null);
 const selectedRow = ref(null);
 
 const purchasedPrice = ref(null);
 const retailPrice = ref(null);
-
+const totalItems = ref(0);
 
 const columns = [
   {
@@ -113,6 +190,13 @@ const columns = [
     sortable: true,
   },
   {
+    name: "profit",
+    align: "center",
+    label: "Profit",
+    field: "profit",
+    sortable: true,
+  },
+  {
     name: "remaining",
     label: "Remaining",
     field: "remaining",
@@ -123,12 +207,11 @@ const columns = [
   { name: "Action", align: "center", label: "Sell Item", field: "action" },
 ];
 
-
 function handleRowClick(evt, row, index) {
   console.log(evt, row, index);
 
   isRow.value = true;
-  selectedRow.value = row
+  selectedRow.value = row;
 }
 
 // Export data to CSV
@@ -170,7 +253,8 @@ async function getData() {
           product_name: item.product_name,
           retail_price: "Rs. " + item.retail_price + " /-",
           purchased_price: "Rs. " + item.purchased_price + " /-",
-          remaining: item.total_items,
+          profit: item.retail_price - item.purchased_price,
+          remaining: item.total_items + " Items",
         });
       });
     });
@@ -180,12 +264,13 @@ let filterData = computed(() => {
   if (!searchProduct.value.trim()) {
     return rows.value;
   }
-  return rows.value.filter((item) =>
-    item.product_name.toLowerCase().includes(searchProduct.value.toLowerCase()) || item.retail_price.includes(searchProduct.value.toLocaleLowerCase())
+  return rows.value.filter(
+    (item) =>
+      item.product_name.toLowerCase().includes(searchProduct.value.toLowerCase()) ||
+      item.retail_price.includes(searchProduct.value.toLocaleLowerCase())
   );
 });
 filterData;
-
 
 // Toggle sell input for a specific row
 function toggleSellInput(row) {
@@ -194,47 +279,41 @@ function toggleSellInput(row) {
     r.isSell = r === row ? !r.isSell : false;
   });
   sellingId.value = row.sno;
-
 }
-
 
 const handleSellItems = async (row) => {
   let formData = new FormData();
-  formData.append('soldItems', sellItems.value)
+  formData.append("soldItems", sellItems.value);
   formData.append("id", sellingId.value);
 
   await fetch("http://localhost:8000/soldItem.php", {
     method: "POST",
-    body: formData
-  }).then( (res) => res).then(async (result) => {
-    console.log(result);
+    body: formData,
+  })
+    .then((res) => res)
+    .then(async (result) => {
+      console.log(result);
 
-    sellItems.value = null;
-    rows.value.forEach((r) => {
-      r.isSell = r === row ? !r.isSell : false;
+      sellItems.value = null;
+      rows.value.forEach((r) => {
+        r.isSell = r === row ? !r.isSell : false;
+      });
+      $q.notify({ color: "green", message: `Sold Successfully !!`, position: "top" });
+      // const soldItemsValue = parseInt(sellItems.value, 10);
+      // if (!isNaN(soldItemsValue)) {
+      //   row.remaining -= soldItemsValue; // Adjust the remaining count
+      // }
+    })
+    .catch((error) => {
+      console.log(error);
+      $q.notify({ color: "red", message: `Failed !! `, position: "top" });
+    })
+    .then(() => {
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
     });
-    $q.notify({ color: "green", message: `Sold Successfully !!` , position : 'top'});
-    // const soldItemsValue = parseInt(sellItems.value, 10);
-    // if (!isNaN(soldItemsValue)) {
-    //   row.remaining -= soldItemsValue; // Adjust the remaining count
-    // }
-  }).catch(error => {
-
-    console.log(error)
-    $q.notify({ color: "red", message: `Failed !! ` , position : 'top'});
-  }).then(() => {
-    setTimeout(() => {
-
-      window.location.reload();
-    } , 1000)
-
-  });
-
-
-
-}
-
-
+};
 
 const handleUpdatePrice = async () => {
   if (!selectedRow.value) return;
@@ -243,7 +322,7 @@ const handleUpdatePrice = async () => {
   formData.append("id", selectedRow.value.sno); // Pass the row ID
   formData.append("purchased_price", purchasedPrice.value);
   formData.append("retail_price", retailPrice.value);
-
+  formData.append("total_items", totalItems.value);
   try {
     const response = await fetch("http://localhost:8000/update.php", {
       method: "POST",
@@ -257,29 +336,31 @@ const handleUpdatePrice = async () => {
     // Update the row data locally
     selectedRow.value.purchased_price = `Rs. ${purchasedPrice.value} /-`;
     selectedRow.value.retail_price = `Rs. ${retailPrice.value} /-`;
+    selectedRow.value.remaining = `${totalItems.value} Items`;
 
-    $q.notify({ color: "green", message: "Price updated successfully!", position: "top" });
+    $q.notify({
+      color: "green",
+      message: "Price updated successfully!",
+      position: "top",
+    });
+    window.location.reload();
   } catch (error) {
     console.error(error);
     $q.notify({ color: "red", message: "Failed to update price!", position: "top" });
   } finally {
     isRow.value = false; // Close the modal
-
-
   }
 };
 
 onMounted(async () => {
   await getData();
 });
-
-
 </script>
 
-
-
 <style scoped>
-   tbody, tr , td{
-    font-size: 50px;
-  }
+tbody,
+tr,
+td {
+  font-size: 50px;
+}
 </style>
